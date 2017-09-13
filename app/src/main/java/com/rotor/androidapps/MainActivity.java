ae6f4e549d80314ff505105a9fc27badf0fad058
Jorge Atempa <atempaitm@gmail.com>
2017-09-12 19:34:11 -0700

    Reset the project

--- app/src/main/java/com/rotor/androidapps/MainActivity.java
+++ app/src/main/java/com/rotor/androidapps/MainActivity.java
@@ -10,42 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "onCreate method", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Toast.makeText(this, "onCreate method", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(this, "onResume method", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onPostResume() {
-        super.onPostResume();
-        Toast.makeText(this, "onPostResume method", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(this, "onPause method", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(this, "onStop method", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(this, "onDestroy method", Toast.LENGTH_SHORT).show();
     }
 }
