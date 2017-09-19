8355a45ac8387a02cb0d086d9d03aaba6a6275d3
Jorge Atempa <atempaitm@gmail.com>
2017-09-18 19:41:54 -0700

    Placeholder for the methods 'QuickSort' and 'MergeSort'

--- app/src/test/java/com/rotor/androidapps/ExampleUnitTest.java
+++ app/src/test/java/com/rotor/androidapps/ExampleUnitTest.java
@@ -19,31 +19,29 @@ import static org.mockito.Mockito.when;
  */
 public class ExampleUnitTest {
     @Test
-    public void testLinkedList()  {
-        LinkedList mockedList = mock(LinkedList.class);
-        // stubbing appears before the actual execution
-        when(mockedList.get(0)).thenReturn("first");
-
-        Assert.assertEquals("first", mockedList.get(0));
-    }
-
-    @Test
     public void testSortMethodsInterface()  {
         SortMethods example = mock(SortMethods.class);
         int[] arrayDemo = { 3,52,12,94,83 };
         int[] arraySorted = { 3,12,52,83,94 };
         when(example.sort(arrayDemo)).thenReturn(arraySorted);
-
         Assert.assertEquals(arraySorted, example.sort(arrayDemo));
     }
 
     @Test
     public void testBubbleSortMethod()  {
-        SortMethods example = mock(BubbleSort.class);
+        SortMethods example = new BubbleSort();
         int[] arrayDemo = { 3,52,12,94,83 };
-        int[] arraySorted = { 3,12,52,83,94 };
-        when(example.sort(arrayDemo)).thenReturn(new int[]{3,12,52,83,94});
         int[] result = example.sort(arrayDemo);
-        Assert.assertEquals(94, result[4]);
+        Assert.assertTrue(94 == result[4]);
+    }
+
+    @Test
+    public void testMergeSortMethod()  {
+        /* implementation */
+    }
+
+    @Test
+    public void testQuickSortMethod()  {
+        /* implementation */
     }
 }
